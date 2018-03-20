4fe7527250ee45eecc40a86d6e1f88e7d979af13
Dmitry Pokidov <dooman87@gmail.com>
2018-03-21 08:57:15 +1100

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+go run main.go
+
+if [ ! -d "./dist" ]; then
+    echo "Dist directory doesn't exist, exiting..."
+    exit 1
+fi
+
+gsutil cp -r dist/web/* gs://www.midday.coffee
+gsutil iam ch allUsers:objectViewer gs://www.midday.coffee
